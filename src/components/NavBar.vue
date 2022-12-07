3e4ad0b133a4131721bd4103e678d4d3ad83ab89
kemisolaola <kemisola077@gmail.com>
2022-12-07 10:09:12 +0100

    add home -icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,6 +3,13 @@
 <div class="logo-container blue-gradient d-flex align-center justify-center">
     <img src="@/assets/eazipay.svg" alt="" height="45%">
 </div>
+<div class="mt-7">
+  <router-link to="/" class="d-flex align-center justify-center" active-class="active">
+    <img class="pr-2" src="@/assets/home-icon.svg"/>
+      <p class="text-center mb-0">DASHBOARD</p>
+    </router-link>
+    <v-divider class="mt-3"></v-divider>
+</div>
   </v-navigation-drawer>
 </template>
 <script>
@@ -17,5 +24,14 @@ export default {
 <style scoped>
 .logo-container {
     height: 150px;
+}
+.active{
+  color: #E4572E
+}
+a {
+  text-decoration: none;
+  font-weight: 700;
+font-size: 16px;
+line-height: 19px;
 }
 </style>
